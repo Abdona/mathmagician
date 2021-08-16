8d46ee254eaa2cceaa7f885581208c24a63ddbbe
Abdulrahman <boodynaser@rocketmail.com>
2021-08-16 14:10:14 +0200

    fix class components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import MyCalculator from './components/Calculator';
 
-class App extends React.Component {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = '';
+  }
+
   render() {
     return <MyCalculator />;
   }
 }
-
-export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,31 +1,34 @@
-/* eslint-disable react/button-has-type */
-/* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 
 export default class MyCalculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = '';
+  }
+
   render() {
     return (
       <div className="grid-continer">
         <input className="resultscreen" placeholder="0" />
-        <button>AC</button>
-        <button>+/-</button>
-        <button>%</button>
-        <button className="buttoperation">/</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button className="buttoperation">x</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button className="buttoperation">-</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button className="buttoperation">+</button>
-        <button className="zeroitem">0</button>
-        <button>.</button>
-        <button className="buttoperation">=</button>
+        <button type="button">AC</button>
+        <button type="button">+/-</button>
+        <button type="button">%</button>
+        <button type="button" className="buttoperation">/</button>
+        <button type="button">7</button>
+        <button type="button">8</button>
+        <button type="button">9</button>
+        <button type="button" className="buttoperation">x</button>
+        <button type="button">4</button>
+        <button type="button">5</button>
+        <button type="button">6</button>
+        <button type="button" className="buttoperation">-</button>
+        <button type="button">1</button>
+        <button type="button">2</button>
+        <button type="button">3</button>
+        <button type="button" className="buttoperation">+</button>
+        <button type="button" className="zeroitem">0</button>
+        <button type="button">.</button>
+        <button type="button" className="buttoperation">=</button>
       </div>
     );
   }
