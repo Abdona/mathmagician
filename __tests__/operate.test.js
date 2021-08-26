2159a556fed88313df8ba6b84fe4d756bbe94681
Abdulrahman <boodynaser@rocketmail.com>
2021-08-26 15:29:10 +0200

    Add subtract test

--- __tests__/operate.test.js
+++ __tests__/operate.test.js
@@ -3,7 +3,11 @@ import operate from '../src/logic/operate';
 const fnum = 5;
 const snum = 10;
 describe('Test operate function', () => {
-  test('add operation return String result', () => {
+  test('add operation return String 15 as a result', () => {
     expect(operate(fnum, snum, '+')).toEqual('15');
+  });
+
+  test('subtract operation return String -5 as a result', () => {
+    expect(operate(fnum, snum, '-')).toEqual('-5');
   });
 });
